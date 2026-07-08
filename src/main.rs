a74e30e958756e5874724883409ccc12bda0d6f7
Benjamin Kane <6081085+bbkane@users.noreply.github.com>
2026-07-08 14:45:11 -0700

    Add skeleton clap CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Args)]
+struct HelloArgs {
+    #[arg(long)]
+    name: String,
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Print version")]
+    Version,
+
+    #[command(about = "Say hello")]
+    Hello(HelloArgs),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match &args.command {
+        Commands::Version => {
+            println!("v{}", env!("CARGO_PKG_VERSION"));
+        }
+        Commands::Hello(args) => {
+            println!("Hello {}!", args.name);
+        }
+    }
 }
